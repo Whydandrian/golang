5e07120870d53f42250c14c7871935971cb9ef1a
whydandrian <whydandrian@gmail.com>
2021-08-02 20:00:22 +0700

    add file math-operation and add code for it

---
+++ math-operation.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main()  {
+	var result = 20
+	var a = 10
+	var b = 3
+	var c = a * b	
+
+	fmt.Println(c)
+	fmt.Println(result)
+	
+	var i = 20
+
+	fmt.Println(i)
+}
