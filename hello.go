ef849b52bb617efa0fd040c4578bd9a82360359a
whydandrian <whydandrian@gmail.com>
2021-08-02 18:38:01 +0700

    add for loop

--- hello.go
+++ hello.go
@@ -1,8 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main()  {
-	fmt.Println("Hi.. I'm Wahyudi Andrian")
-	fmt.Println("I'm from Lumajang, East Java")
+	// fmt.Println("Hi.. I'm Wahyudi Andrian")
+	// fmt.Println("I'm from Lumajang, East Java")
+
+	var s string
+	sep := ""
+	for _, arg := range os.Args[1:] {
+		s += sep + arg
+		sep = " "
+	}
+	fmt.Println((s))
 }
