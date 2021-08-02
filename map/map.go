f9b37751494fe295bf29a91848569847107fe1b4
whydandrian <whydandrian@gmail.com>
2021-08-03 00:23:23 +0700

    modify: implement function make & delete map

--- map/map.go
+++ map/map.go
@@ -13,5 +13,14 @@ func main()  {
 	fmt.Println(person["name"])
 	fmt.Println(person["address"])
 	fmt.Println(person["title"])
+
+	var book map[string]string = make(map[string]string)
+	book["title"] = "Belajar Go-Lang"
+	book["author"] = "Wahyudi"
+	book["status"] = "released"
+
+	fmt.Println(book)
 	
+	delete(book, "status")
+	fmt.Println(book)
 }
