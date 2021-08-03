c4f1d704cfd96617acbb7349a31ad2f12121aef7
whydandrian <whydandrian@gmail.com>
2021-08-03 23:14:27 +0700

    implement switch with condition

--- condition/switch.go
+++ condition/switch.go
@@ -15,10 +15,20 @@ func main() {
 		fmt.Println("Nama lengkap yaa")
 	}
 
-	switch length := len(name); length > 5 {
-	case true:
+	// switch length := len(name); length > 5 {
+	// case true:
+	// 	fmt.Println("Nama terlalu panjang")
+	// case false:
+	// 	fmt.Println("Nama sudah benar")
+	// }
+
+	length := len(name)
+	switch {
+	case length > 10:
 		fmt.Println("Nama terlalu panjang")
-	case false:
+	case length > 5:
+		fmt.Println("Nama lumayan panjang")
+	default :
 		fmt.Println("Nama sudah benar")
 	}
 }
