cec29c9b72835aba835f895cb1042aadff1f3e33
whydandrian <whydandrian@gmail.com>
2021-08-03 22:52:38 +0700

    add file if.go and implement ifelse condition

---
+++ condition/if.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name = "Wahyudi"
+
+	if name == "Wahyudi" {
+		fmt.Println("Hello Wahyudi")
+	} else {
+		fmt.Println("kamu  bukan wahyudi")
+	}
+}
