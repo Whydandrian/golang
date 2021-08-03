b68134ebc7720a16f4254531eeb302d2fa2af069
whydandrian <whydandrian@gmail.com>
2021-08-03 22:58:03 +0700

    modify: add short statement if golang

--- condition/if.go
+++ condition/if.go
@@ -10,4 +10,11 @@ func main() {
 	} else {
 		fmt.Println("kamu  bukan wahyudi")
 	}
+
+	//Short statement
+	if length := len(name); length > 5 {
+		fmt.Println("Nama terlalu panjang")
+	} else {
+		fmt.Println("Nama sudah benar")
+	}
 }
