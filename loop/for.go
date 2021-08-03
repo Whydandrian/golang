ce4bd44184c82ca42feb6d826cf0a2c0cd5fa4c2
whydandrian <whydandrian@gmail.com>
2021-08-03 23:53:09 +0700

    implement short for range

--- loop/for.go
+++ loop/for.go
@@ -13,8 +13,12 @@ func main() {
 	// For range with slice
 	slice := []string{"wahyudi", "andrian","ganteng"}
 
-	for i := 0; i < len(slice); i++ {
-		fmt.Println(slice[i])
+	// for i := 0; i < len(slice); i++ {
+	// 	fmt.Println(slice[i])
+	// }
+
+	for i, value := range slice {
+		fmt.Println("Index ke ", i, "= ", value)
 	}
 
 }
