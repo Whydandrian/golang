df3956d30c2e5b30971774c102bf8d97574d13a6
whydandrian <whydandrian@gmail.com>
2021-08-04 22:40:34 +0700

    implement continue on for loop

--- advance_loop/break-continue.go
+++ advance_loop/break-continue.go
@@ -4,8 +4,13 @@ import "fmt"
 
 func main() {
 	for i := 0; i < 10; i++ {
-		if i == 5 {
-			break
+		// Break for loop
+		// if i == 5 {
+		// 	break
+		// }
+
+		if i%2 == 0 {
+			continue
 		}
 		fmt.Println("No. ", i)
 	}
