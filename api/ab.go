321cc465b462ce408a442ace85a1655bfda21bea
whydandrian <whydandrian@gmail.com>
2021-08-05 23:26:36 +0700

    remove an api key

--- api/ab.go
+++ api/ab.go
@@ -2,15 +2,15 @@ package main
 
 import (
 	"fmt"
-	"net/http"
 	"io/ioutil"
+	"net/http"
 )
 
 func main() {
 	url := "https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=libra&day=today"
 	req, _ := http.NewRequest("POST", url, nil)
 
-	req.Header.Add("x-rapidapi-key", "ed5f837f73msh394956de50910e0p1908e5jsn80aa43a20b2c")
+	req.Header.Add("x-rapidapi-key", "")
 	req.Header.Add("x-rapidapi-host", "sameer-kumar-aztro-v1.p.rapidapi.com")
 
 	res, _ := http.DefaultClient.Do(req)
