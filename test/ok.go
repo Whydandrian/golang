2ec231c538151bd051de264fc7d4e712fcc8abdf
whydandrian <whydandrian@gmail.com>
2021-08-06 21:03:42 +0700

    get data type and implement to get pointer variables

--- test/ok.go
+++ test/ok.go
@@ -2,18 +2,49 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"math/rand"
+	"strconv"
 )
 
 func main() {
-	a := rand.Intn(56)
+	// variable standard
 
+	a := rand.Intn(56)
 	fmt.Println("Nomor favorit saya adalah :", a)
 
+	// declare variable in 1 row
 	c, python, java := true, false, "no!"
-
 	fmt.Println(c, python, java)
 
+	// convert data type
+	myNumber := 100
+	num := strconv.Itoa(myNumber)
+	fmt.Println("Hasil dari konvert data tipe integer ke string adalah : ", num)
 
+	x, y := 3, 4
+	result := math.Sqrt(float64((x*x + y*y)))
+	fmt.Println(result)
 
+	z := uint(result)
+
+	println(x, y, z)
+
+	g := "tuet"
+	uui := 123.2
+	in := 12
+
+	fmt.Printf("Tipe data : %T\n", g)
+	fmt.Printf("Tipe data : %T\n", uui)
+	fmt.Printf("Tipe data : %T\n", in)
+	
+	// buatlah variabel p dengan pointer i
+	ii, jj := 42, 2701
+	p := &ii
+	fmt.Println(*p)
+	*p = 21
+	fmt.Println(ii)
+	p = &jj
+	*p = *p / 37
+	fmt.Println(jj)
 }
