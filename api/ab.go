1a4c1d7c57b6536d196d738f41509edd021121bd
whydandrian <whydandrian@gmail.com>
2021-08-07 11:27:56 +0700

    add file method, function, struct

--- api/ab.go
+++ api/ab.go
@@ -10,7 +10,7 @@ func main() {
 	url := "https://sameer-kumar-aztro-v1.p.rapidapi.com/?sign=libra&day=today"
 	req, _ := http.NewRequest("POST", url, nil)
 
-	req.Header.Add("x-rapidapi-key", "")
+	req.Header.Add("x-rapidapi-key", "ed5f837f73msh394956de50910e0p1908e5jsn80aa43a20b2c")
 	req.Header.Add("x-rapidapi-host", "sameer-kumar-aztro-v1.p.rapidapi.com")
 
 	res, _ := http.DefaultClient.Do(req)
