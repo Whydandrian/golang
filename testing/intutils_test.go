32f34bab5142f057f7e095a779710906d2ad6ae8
whydandrian <whydandrian@gmail.com>
2021-08-08 13:03:39 +0700

    modify code testing

--- testing/intutils_test.go
+++ testing/intutils_test.go
@@ -5,17 +5,16 @@ import (
     "testing"
 )
 
-func IntMin(a, b int) int {
-    if a < b {
-        return a
-    }
-    return b
-}
+// func IntMin(a, b int) int {
+//     if a < b {
+//         return a
+//     }
+//     return b
+// }
 
 func TestIntMinBasic(t *testing.T) {
     ans := IntMin(2, -2)
     if ans != -2 {
-
         t.Errorf("IntMin(2, -2) = %d; want -2", ans)
     }
 }
