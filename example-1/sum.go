150d16bb9ee312a31208e3b785e79ac33fa7c730
whydandrian <whydandrian@gmail.com>
2021-08-08 11:56:47 +0700

    add example testing

--- example-1/sum.go
+++ example-1/sum.go
@@ -1,5 +1,13 @@
 package main
 
-func Sum(x, y int) int  {
+func Sum(x, y int) int {
 	return x + y
+}
+
+func SumVariadic(nums ...int) int {
+	total := 0
+	for _, num := range nums {
+		total += num
+	}
+	return total
 }
--- example-1/sum_test.go
+++ example-1/sum_test.go
@@ -5,7 +5,7 @@ import (
 	"testing"
 )
 
-func TestSum(t *testing.T)  {
+func TestSum(t *testing.T) {
 	res := Sum(5, 5)
 
 	if res != 10 {
@@ -13,26 +13,35 @@ func TestSum(t *testing.T)  {
 	}
 }
 
+func TestSumVariadic(t *testing.T) {
+	sumTest := SumVariadic(1, 3)
+	
+	if sumTest != 4 {
+		t.Errorf("SumVariadic(%d, %d) = %d; want 4", 1, 3, sumTest)
+	}
+
+}
+
 func TestSumTableDriven(t *testing.T) {
 	var tests = []struct {
-			x, y int
-			res int
+		x, y int
+		res  int
 	}{
-			{1, 2, 3},
-			{1, 0, 1},
-			{2, 3, 5},
-			{2, 2, 4},
-			{4, 3, 7},
+		{1, 2, 3},
+		{1, 0, 1},
+		{2, 3, 5},
+		{2, 2, 4},
+		{4, 3, 7},
 	}
 
 	for _, tt := range tests {
 
-			testname := fmt.Sprintf("%d,%d", tt.x, tt.y)
-			t.Run(testname, func(t *testing.T) {
-					result := Sum(tt.x, tt.y)
-					if result != tt.res {
-							t.Errorf("result is %d, want %d", result, tt.res)
-					}
-			})
+		testname := fmt.Sprintf("%d,%d", tt.x, tt.y)
+		t.Run(testname, func(t *testing.T) {
+			result := Sum(tt.x, tt.y)
+			if result != tt.res {
+				t.Errorf("result is %d, want %d", result, tt.res)
+			}
+		})
 	}
 }
