34fe4ecd8c4c16f5054b196faa6b8f3d604a4dd1
whydandrian <whydandrian@gmail.com>
2021-08-10 19:54:01 +0700

    add slice handons

---
+++ slices/handson/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	// buat variabel primes dan buatlah
+	// menggunakan slice literals dengan panjang 6
+	// buat 6 angka bilangan prima
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	// print primes
+fmt.Println(primes)
+// print primes dari data ke 3 sampai 5
+fmt.Println(primes[3:5])
+
+
+}
