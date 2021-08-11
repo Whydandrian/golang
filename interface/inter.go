88f81c95eba2581b7c6a6d0594fa73b0d5733eb6
whydandrian <whydandrian@gmail.com>
2021-08-11 22:33:37 +0700

    add inter.go & implement interface person and animal

---
+++ interface/inter.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+type HasName interface {
+	GetName() string
+}
+
+func SayHello(hasName HasName) {
+	fmt.Println("Hello", hasName.GetName())
+}
+
+type Person struct {
+	Name string
+}
+
+type Animal struct {
+	Name string
+}
+
+func (person Person) GetName() string  {
+	return person.Name
+}
+func (animal Animal) GetName() string  {
+	return animal.Name
+}
+
+func main() {
+var wahyudi Person
+var cat Animal
+wahyudi.Name = "whydandrian"
+cat.Name = "meoongg"
+
+SayHello(wahyudi)
+SayHello(cat)
+}
