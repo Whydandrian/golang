943b425f7024e32ae07eb4b9fc24d34bbed720ee
whydandrian <whydandrian@gmail.com>
2021-08-11 21:42:20 +0700

    add strc.go

--- struct/strc.go
+++ struct/strc.go
@@ -5,6 +5,7 @@ import "fmt"
 type Customer struct {
 	Name, Address string
 	Age           int
+	Married bool
 }
 
 func main() {
@@ -17,4 +18,14 @@ func main() {
 	fmt.Println(wahyudi.Name)
 	fmt.Println(wahyudi.Address)
 	fmt.Println(wahyudi.Age)
+
+	whyd := Customer {
+		Name: "whydandrian",
+		Address: "Lumajang-Jatim",
+		Age: 24,
+	}
+	fmt.Println(whyd)
+	// kode di bawah ini akan error karena deklarasi value tidak sesuai dg
+	// urutan field struct Customer
+	// yudi := Customer{"Yuyud", "magelang", 22}
 }
